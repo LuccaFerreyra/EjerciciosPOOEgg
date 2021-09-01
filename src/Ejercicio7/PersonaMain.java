96768a7a13a696d9ee77968909809f83198806a1
Lucca Ferreyra <lucca.ferreyra@hotmail.com>
2021-09-01 10:37:21 -0300

    JavaPOO

---
+++ src/Ejercicio7/PersonaMain.java
@@ -0,0 +1,13 @@
+package ejercicio7;
+
+public class PersonaMain {
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        persona1.crearPersona(persona1);
+        persona1.esMayorDeEdad(persona1);
+        persona1.calcularIMC(persona1);
+
+    }
+
+}
